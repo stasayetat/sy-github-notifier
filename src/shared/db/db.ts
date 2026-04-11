9c038160e5dd9e93b76cfc68354f92170ffab87a
stasayetat <stanislav.yarets@flatratetickets.com>
2026-04-11 14:18:55 +0300

    refactor: check env.POSTGRES_URL

--- src/shared/db/db.ts
+++ src/shared/db/db.ts
@@ -1,6 +1,8 @@
 import { env } from '@shared/env';
+import { logger } from '@shared/logger';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
+logger.info(`DB: ${env.POSTGRES_URL}`);
 const client = postgres(env.POSTGRES_URL);
 export const db = drizzle(client);
