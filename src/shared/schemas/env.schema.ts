02cefc168097f77bb5f30e13f7e948192abbff34
stasayetat <stanislav.yarets@flatratetickets.com>
2026-04-11 14:08:19 +0300

    refactor: remove `HOST` from env

--- src/shared/schemas/env.schema.ts
+++ src/shared/schemas/env.schema.ts
@@ -6,7 +6,6 @@ export const EnvironmentSchema = zod.object({
   GRPC_PORT: zod.coerce.number(),
   NODE_ENV: zod.enum(['development', 'production', 'test', 'ci']),
   APP_URL: zod.string(),
-  HOST: zod.string(),
   POSTGRES_URL: zod.string(),
   GITHUB_AUTH_TOKEN: zod.string(),
   RESEND_AUTH_TOKEN: zod.string(),
