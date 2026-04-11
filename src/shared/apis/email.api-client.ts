5f0860c6148f4f5b3df40a113c47e6725dabd4ac
stasayetat <stanislav.yarets@flatratetickets.com>
2026-04-11 20:35:37 +0300

    refactor: use resend lib for prod

--- src/shared/apis/email.api-client.ts
+++ src/shared/apis/email.api-client.ts
@@ -3,6 +3,7 @@ import { logger } from '@shared/logger';
 import { getErrorMessage } from '@shared/utils';
 import Bottleneck from 'bottleneck';
 import nodemailer from 'nodemailer';
+import { Resend } from 'resend';
 
 export namespace EmailApiClient {
   const limiter = new Bottleneck({
@@ -19,19 +20,37 @@ export namespace EmailApiClient {
     },
   });
 
+  const resend = new Resend(env.SMTP_PASS);
+
+  const sendViaResend = (recipientEmail: string, subject: string, htmlText: string) => {
+    return limiter.schedule(() =>
+      resend.emails.send({
+        from: env.SMTP_SENDER_EMAIL,
+        to: recipientEmail,
+        subject,
+        html: htmlText,
+      }),
+    );
+  };
+
+  const sendViaSmtp = (recipientEmail: string, subject: string, htmlText: string) => {
+    return transporter.sendMail({
+      from: env.SMTP_SENDER_EMAIL,
+      to: recipientEmail,
+      subject,
+      html: htmlText,
+    });
+  };
+
   export const sendEmail = async (recipientEmail: string, subject: string, htmlText: string) => {
     try {
-      await limiter.schedule(() => {
-        return transporter.sendMail({
-          from: env.SMTP_SENDER_EMAIL,
-          to: recipientEmail,
-          subject,
-          html: htmlText,
-        });
-      });
+      if (env.NODE_ENV === 'production') {
+        await sendViaResend(recipientEmail, subject, htmlText);
+      } else {
+        await sendViaSmtp(recipientEmail, subject, htmlText);
+      }
     } catch (error) {
-      logger.error(`Error sending email using nodemail: ${getErrorMessage(error)}`);
-
+      logger.error(`Error sending email: ${getErrorMessage(error)}`);
       throw error;
     }
   };
